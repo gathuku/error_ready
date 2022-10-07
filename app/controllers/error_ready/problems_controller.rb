521f66e5e42d47b80a74f402411cc2ff0b98283e
Moses Gathuku <mosesgathuku95@gmail.com>
2022-10-07 11:11:51 +0300

    refine index page

--- app/controllers/error_ready/problems_controller.rb
+++ app/controllers/error_ready/problems_controller.rb
@@ -1,43 +1,14 @@
 module ErrorReady
   class ProblemsController < ApplicationController
-    before_action :set_problem, only: %i[ show edit update destroy ]
+    before_action :set_problem, only: %i[ show destroy ]
 
     # GET /problems
     def index
-      @problems = Problem.all
+      @problems = Problem.latest_first
     end
 
     # GET /problems/1
     def show
-    end
-
-    # GET /problems/new
-    def new
-      @problem = Problem.new
-    end
-
-    # GET /problems/1/edit
-    def edit
-    end
-
-    # POST /problems
-    def create
-      @problem = Problem.new(problem_params)
-
-      if @problem.save
-        redirect_to @problem, notice: "Problem was successfully created."
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    # PATCH/PUT /problems/1
-    def update
-      if @problem.update(problem_params)
-        redirect_to @problem, notice: "Problem was successfully updated."
-      else
-        render :edit, status: :unprocessable_entity
-      end
     end
 
     # DELETE /problems/1
@@ -50,11 +21,6 @@ module ErrorReady
       # Use callbacks to share common setup or constraints between actions.
       def set_problem
         @problem = Problem.find(params[:id])
-      end
-
-      # Only allow a list of trusted parameters through.
-      def problem_params
-        params.require(:problem).permit(:message, :err_class, :environment, :first_notice_at, :last_notice_at, :notices_count, :resolved_at)
       end
   end
 end
