610e9947716d5cdacc498589d0f5fb085764b825
Moses Gathuku <mosesgathuku95@gmail.com>
2022-11-17 00:18:42 +0300

    Expand exception (#9)
    * use exception wrapper to extract new fields
    
    * rearrange format keys
    
    * update error file

--- lib/error_ready/formatter.rb
+++ lib/error_ready/formatter.rb
@@ -1,23 +1,32 @@
 module ErrorReady
   class Formatter
     def initialize(error:, handled:, severity:, context:, source: nil)
-      @error = error
       @handled = handled
       @severity = severity
       @context = context
       @source = source
+      @error = wrapped_exception(error)
     end
 
     def format
       {
-        message: @error.message,
-        err_class: @error.class.to_s,
-        backtrace: @error.backtrace,
+        message: @error.exception.message,
+        err_class: @error.exception.class.to_s,
         severity: @severity.to_s,
-        context: @context.transform_values(&:to_s),
         source: @source,
-        environment: Rails.env
+        environment: Rails.env,
+        context: @context.transform_values(&:to_s),
+        source_to_show: @error.source_extracts[@error.source_to_show_id],
+        application_trace: @error.application_trace,
+        full_trace: @error.full_trace
       }
+    end
+
+    def wrapped_exception(error)
+      ActionDispatch::ExceptionWrapper.new(
+        Rails::BacktraceCleaner.new,
+        error
+      )
     end
   end
 end
